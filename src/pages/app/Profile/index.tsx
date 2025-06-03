1f30fec0bcf8ab549bf01d4cb26b1b6757a01fa3
miguelscastro <miguelscastro18@gmail.com>
2025-06-03 16:57:34 -0300

    feat: add profile page to redirect to after sign_in

---
+++ src/pages/app/Profile/index.tsx
@@ -0,0 +1,68 @@
+import {
+  ListIcon,
+  ShoppingBagIcon,
+  SignOutIcon,
+  UserCircleIcon,
+} from '@phosphor-icons/react'
+import { Container, Info, Menu } from './styles'
+import { useState } from 'react'
+import { Purchases } from './components/Purchases'
+import { MyProfile } from './components/MyProfile'
+import { useAuth } from '../../../hooks/useAuth'
+import { useNavigate } from 'react-router-dom'
+
+export function Profile() {
+  const { logout } = useAuth()
+  const navigate = useNavigate()
+  const [selectedItem, setSelectedItem] = useState<'purchases' | 'profile'>(
+    'profile',
+  )
+
+  const pages = {
+    purchases: <Purchases />,
+    profile: <MyProfile />,
+  }
+
+  return (
+    <Container>
+      <Menu>
+        <div>
+          <ListIcon size={30} />
+          <span>Minha conta</span>
+        </div>
+        <ul>
+          <li>
+            <button
+              onClick={() => setSelectedItem('purchases')}
+              className={selectedItem === 'purchases' ? 'active' : ''}
+            >
+              <ShoppingBagIcon size={30} />
+              Compras
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => setSelectedItem('profile')}
+              className={selectedItem === 'profile' ? 'active' : ''}
+            >
+              <UserCircleIcon size={30} />
+              Meu perfil
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                logout()
+                navigate('/')
+              }}
+            >
+              <SignOutIcon size={30} />
+              Sair
+            </button>
+          </li>
+        </ul>
+      </Menu>
+      <Info>{pages[selectedItem]}</Info>
+    </Container>
+  )
+}
---
+++ src/pages/app/Profile/styles.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+import { mixins } from '../../../styles/mixins'
+
+export const Container = styled.main`
+  display: flex;
+`
+export const Menu = styled.aside`
+  width: 300px;
+  height: calc(100vh - 104px);
+  background: ${({ theme }) => theme.colors['base-card']};
+
+  > div {
+    display: flex;
+    align-items: flex-end;
+    gap: 1rem;
+
+    padding: 3rem;
+
+    span {
+      ${mixins.fonts.titleXS}
+    }
+  }
+
+  > ul {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    list-style: none;
+
+    li {
+      padding: 0 2.6875rem 0.5rem;
+      border-left: 5px solid transparent;
+
+      &:hover {
+        border-left-color: ${({ theme }) => theme.colors['brown-dark']};
+      }
+
+      > button {
+        display: flex;
+        gap: 0.5rem;
+        align-items: center;
+        width: 100%;
+        padding: 0.5rem 0;
+        border: none;
+        background: transparent;
+        ${mixins.fonts.textS}
+        cursor: pointer;
+
+        transition: border-color 0.1s;
+
+        &.active svg {
+          color: ${({ theme }) => theme.colors['brown-dark']};
+        }
+      }
+    }
+  }
+`
+export const Info = styled.section`
+  flex: 1;
+  height: calc(100vh - 104px);
+  background: ${({ theme }) => theme.colors['base-input']};
+`
