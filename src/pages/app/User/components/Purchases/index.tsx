66a38948b11ca3ed6e7bd810a41ef9c7c07cb4cc
miguelscastro <miguelscastro18@gmail.com>
2025-06-17 11:25:29 -0300

    chore: fix <EmptyOrders />  styles being aplied to <Orders />

--- src/pages/app/User/components/Purchases/index.tsx
+++ src/pages/app/User/components/Purchases/index.tsx
@@ -1,27 +1,27 @@
-import { useEffect } from 'react'
-import { useCart } from '../../../../../hooks/useCart'
-import { Order } from './components/Order'
-import { Container } from './styles'
-import { EmptyIcon } from '@phosphor-icons/react'
+import { useEffect } from "react";
+import { useCart } from "../../../../../hooks/useCart";
+import { Order } from "./components/Order";
+import { Container, EmptyOrders } from "./styles";
+import { EmptyIcon } from "@phosphor-icons/react";
 
 export function Purchases() {
-  const { orders, getOrders } = useCart()
+  const { orders, getOrders } = useCart();
 
   useEffect(() => {
     if (orders.length === 0) {
-      getOrders()
+      getOrders();
     }
-  }, [])
+  }, []);
 
   return (
     <Container>
       <div>
         <h1>Seus pedidos</h1>
         {orders.length === 0 ? (
-          <div>
+          <EmptyOrders>
             <h2>Você ainda não fez nenhuma compra</h2>
             <EmptyIcon />
-          </div>
+          </EmptyOrders>
         ) : (
           orders.map((orderItems, index) => (
             <Order key={index} order={orderItems} />
@@ -29,5 +29,5 @@ export function Purchases() {
         )}
       </div>
     </Container>
-  )
+  );
 }
--- src/pages/app/User/components/Purchases/styles.ts
+++ src/pages/app/User/components/Purchases/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
   max-width: 1440px;
@@ -16,41 +16,42 @@ export const Container = styled.div`
       margin-bottom: 1rem;
     }
 
-    > div {
-      margin-top: 3rem;
-      padding: 5rem 5rem 4rem;
-      text-align: center;
-      box-shadow: 0 2px 6px ${({ theme }) => theme.colors['base-text']}22;
-      opacity: 0.5;
-
-      border: 2px solid black;
-      border-radius: 2rem;
-
-      > svg {
-        width: 220px;
-        height: 220px;
-
-        margin-top: 4rem;
-
-        color: ${({ theme }) => theme.colors.error};
-      }
-    }
     &::-webkit-scrollbar {
       width: 0.5rem;
     }
 
     &::-webkit-scrollbar-track {
-      background: ${({ theme }) => theme.colors['base-hover']};
+      background: ${({ theme }) => theme.colors["base-hover"]};
       margin-left: 12px;
     }
 
     &::-webkit-scrollbar-thumb {
-      background-color: ${({ theme }) => theme.colors['base-text']};
+      background-color: ${({ theme }) => theme.colors["base-text"]};
       border-radius: 10px;
     }
 
     &::-webkit-scrollbar-thumb:hover {
-      background-color: ${({ theme }) => theme.colors['brown-dark']};
+      background-color: ${({ theme }) => theme.colors["brown-dark"]};
     }
   }
-`
+`;
+
+export const EmptyOrders = styled.div`
+  margin-top: 3rem;
+  padding: 5rem 5rem 4rem;
+  text-align: center;
+  box-shadow: 0 2px 6px ${({ theme }) => theme.colors["base-text"]}22;
+  opacity: 0.5;
+
+  border: 2px solid black;
+  border-radius: 2rem;
+
+  > svg {
+    width: 220px;
+    height: 220px;
+
+    margin-top: 4rem;
+
+    color: ${({ theme }) => theme.colors.error};
+  }
+`;
